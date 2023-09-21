cc189e735dfb40859305521c04b6f63cf5da3e04
kipyek <deniskipyegon88@gmail.com>
2023-09-21 08:24:08 +0300

    Dummy data

---
+++ DummyData/Data.tsx
@@ -0,0 +1,19 @@
+export const Cover = [
+    {
+        "title": "Excess",
+        "body": "Own damage and partial theft 5% of Sum Insured Minimum Kshs. 15,000" +
+            "Young drivers Kshs. 7,500 additional (under 21 years) " +
+            "Novice drivers Kshs.7,500 additional (Driving experience less than l year) " +
+            "Total theft loss for vehicles with a tracking device 5% of sum insured, minimum Kshs.30,000" +
+            "Total Theft claims (without Anti-theft device)20% of sum insured, minimum Kshs. 30,000 "
+    },
+    {
+        "title": "Cover",
+        "body": "This covers any loss or damage by accidental means, theft, fire, malicious damage to the vehicle and Third Party Liabilities (death, injuries and property damage) in regards to commercial vehicles."
+    },
+    {
+        "title": "Limit of Liability",
+        "body": "Third party bodily injury...Unlimited, Third party property damage...Kshs. 3,000,000 Passenger legal liability..............Kshs. .3,000,000 per person 20,000,000 per event , Towing/ Recovery limit per year ..........Kshs. 30,000 Repair authority per year.......Kshs. 30,000 Emergency medical expenses per year..Kshs. 20,000 Vehicle Entertainment unit ..Kshs.50000 ENTERTAINMENT Windscreen/ window glass replacement cost limit...........Kshs. 50000"
+    }
+
+]
--- src/Home/Quote/QuoteList.tsx
+++ src/Home/Quote/QuoteList.tsx
@@ -1,40 +1,72 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Cover } from '../../../DummyData/Data'
+import { BottomModal, ModalContent } from 'react-native-modals';
 
-const QuoteList = ({onNextStepPressList}:any) => {
+const QuoteList = ({ onNextStepPressList }: any) => {
+  const [modalVisibles, setModalVisibles] = useState(false);
+  console.log(Cover)
 
   const handleQuoteList = () => {
     onNextStepPressList()
   }
   return (
-    <View>
-      <View style={styles.introCard}>
-        <Text className='font-[gothici-Regular]'>Please review the quotes below from different insurers. Click "BUY" against your preferred insurer to add optional benefits.</Text>
-      </View>
+    <Fragment>
+      <View>
+        <View style={styles.introCard}>
+          <Text className='font-[gothici-Regular]'>Please review the quotes below from different insurers. Click "BUY" against your preferred insurer to add optional benefits.</Text>
+        </View>
 
-       <View style={styles.card}>
-      <View className='flex-row items-center'>
-        <Image source={require("../../../assets/images/uap.jpg")} className='w-32 h-32'  />
-        <View className='w-48'>
-          <Text>Insurance company:</Text>
-          <Text >UAP Insurance Company Ltd </Text>
-          <View className='flex-row mt-2'>
-             <Text>Premium: </Text>
-          <Text>184,828</Text>
+        <View style={styles.card}>
+          <View className='flex-row items-center'>
+            <Image source={require("../../../assets/images/uap.jpg")} className='w-32 h-32' />
+            <View className='w-48'>
+              <Text>Insurance company:</Text>
+              <Text >UAP Insurance Company Ltd </Text>
+              <View className='flex-row mt-2'>
+                <Text>Premium: </Text>
+                <Text>184,828</Text>
+
+              </View>
+              <View className='item-center bg-[#302A29] p-1 mt-4 rounded-md '>
+                <TouchableOpacity onPress={() => setModalVisibles(true)}>
+                  <Text className='text-center text-white font-["gothici-Bold"]'>Cover Summary</Text>
+                </TouchableOpacity>
+              </View>
+
+            </View>
           </View>
-         
+
+
+          <View className='item-center bg-[#302A29] p-3 mt-4 rounded-md '>
+            <TouchableOpacity onPress={handleQuoteList}>
+              <Text className='text-center text-white font-["gothici-Bold"]'>BUY</Text>
+            </TouchableOpacity>
+          </View>
+
         </View>
       </View>
+      {/**Cover Summary */}
+      <BottomModal
+        visible={modalVisibles}
+        onTouchOutside={() => setModalVisibles(false)}
+        onHardwareBackPress={() => true}
+        onSwipeOut={() => setModalVisibles(false)}
+      >
+        <ModalContent>
+          <View style={{ borderWidth: 1, width: 50, alignSelf: 'center', marginBottom: 12, borderColor: 'gray' }} />
+          <View>
+            {Cover.map(i =>
+              <View>
+                <Text className='text-center mt-1 mb-1 font-[gothici-Bold]'>{i.title}</Text>
+                <Text>{i.body}</Text>
+              </View>
+            )}
+          </View>
+        </ModalContent>
+      </BottomModal>
+    </Fragment>
 
-      <View className='item-center bg-[#302A29] p-4 mt-4 rounded-md '>
-          <TouchableOpacity onPress={handleQuoteList}>
-            <Text className='text-center text-white font-["gothici-Bold"]'>BUY</Text>
-          </TouchableOpacity>
-      </View>
-      
-    </View> 
-    </View>
-  
   )
 }
 
